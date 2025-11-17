bfca3d008349a230e2120f83676a55e3e9364e4a
cranefirst <3443038221@qq.com>
2025-11-17 20:51:46 +0800

    new

---
+++ mainwindow.cpp
@@ -0,0 +1,20 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->label->setText("lineEdit");
+    ui->label_2->setText("date");
+    ui->label_3->setText("phone");
+    ui->label_4->setText("Identification");
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
